5084857533f678fa410eb9ccdceebde0470bb29b
Rudie Dirkx <github@hotblocks.nl>
2017-11-20 12:21:04 +0100

    example+

---
+++ examples/recorder.php
@@ -0,0 +1,49 @@
+<?php
+
+use JsonStreamingParser\Parser;
+use rdx\jsonpathstreamer\PathAwareJsonListener;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class RecorderParser extends Parser {
+
+}
+
+class SaveAllPathListener extends PathAwareJsonListener {
+
+	public function gotPath(array $path) {
+		// echo implode(' > ', $path) . "\n";
+	}
+
+	public function startObject() {
+		parent::startObject();
+echo "  object\n";
+echo "> " . implode(' > ', $this->path) . "\n\n";
+	}
+
+	public function endObject() {
+		// @todo Keep keeping track of path after closing objects/arrays
+		$this->composeKey();
+
+		parent::endObject();
+
+echo "< " . implode(' > ', $this->path) . "\n\n";
+
+	}
+
+	public function gotValue(array $path, $value) {
+		// var_dump($value);
+	}
+
+}
+
+header('Content-type: text/plain');
+
+$stream = fopen('example.json', 'r');
+
+$listener = new SaveAllPathListener;
+
+$parser = new RecorderParser($stream, $listener);
+$parser->parse();
+
+print_r($listener->getValue());
